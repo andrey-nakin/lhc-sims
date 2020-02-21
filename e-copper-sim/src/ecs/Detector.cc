71c4565a074981dfc378b16e75454f44f286ca5a
Andrey Nakin <andrey@ter-net.com>
2020-02-21 17:30:14 +0300

    work

--- e-copper-sim/src/ecs/Detector.cc
+++ e-copper-sim/src/ecs/Detector.cc
@@ -16,22 +16,22 @@ Detector::Detector() :
 }
 
 G4VPhysicalVolume* Detector::CreateWorld() {
+	using namespace lhcs::repository;
+
 	auto const nist = G4NistManager::Instance();
 	auto const worldSolid = new G4Tubs("WorldSolid", 0. * cm, wordRadius,
 			10. * cm, 0. * deg, 360. * deg);
 	auto const worldLog = new G4LogicalVolume(worldSolid,
 			nist->FindOrBuildMaterial("G4_Galactic"), "World");
-	worldLog->SetVisAttributes(
-			G4VisAttributes(true, lhcs::repository::Colours::Air()));
+	worldLog->SetVisAttributes(G4VisAttributes(true, Colours::Air()));
 
 	auto const targetSolid = new G4Tubs("TargetSolid", 0. * cm, wordRadius,
 			targetWidth / 2, 0. * deg, 360. * deg);
 	auto const targetLog = new G4LogicalVolume(targetSolid,
 			nist->FindOrBuildMaterial("G4_Cu"), "Target");
-	targetLog->SetVisAttributes(
-			G4VisAttributes(lhcs::repository::Colours::Copper()));
-	new G4PVPlacement(nullptr, G4ThreeVector(0., 0., 0.), targetLog,
-			"TargetPhys", worldLog, false, 0);
+	targetLog->SetVisAttributes(G4VisAttributes(Colours::Copper()));
+	new G4PVPlacement(nullptr, G4ThreeVector(0., 0., targetWidth / 2),
+			targetLog, "TargetPhys", worldLog, false, 0);
 
 	auto const worldPhys = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.),
 			worldLog, "WorldPhys", 0, false, 0);
