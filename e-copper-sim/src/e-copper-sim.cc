063446a41e980ed79515da2f536f1585eed6b1b1
Andrey Nakin <andrey@ter-net.com>
2020-02-21 15:24:46 +0300

    work

---
+++ e-copper-sim/src/e-copper-sim.cc
@@ -0,0 +1,14 @@
+#include <G4PhysListFactory.hh>
+
+int main(int argc, char* argv[]) {
+
+	G4cout << G4endl
+			<< "*************************************************************"
+			<< G4endl << "Simulation of electron-induced copper films" << G4endl
+			<< "Copyright Andrey Nakin <andrey.nakin@mail.ru>" << G4endl
+			<< "*************************************************************"
+			<< G4endl << G4endl;
+
+	return 0;
+
+}
