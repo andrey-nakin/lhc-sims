cd1c399d1daa958aa02b9f2b0c265bc533bcfc9e
Andrey Nakin <andrey@ter-net.com>
2020-02-23 17:33:26 +0300

    work

--- e-copper-sim/src/ecs/ActionInitialization.cc
+++ e-copper-sim/src/ecs/ActionInitialization.cc
@@ -1,28 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-
 #include "globals.hh"
 
 #include "ecs/ActionInitialization.hh"
@@ -32,21 +7,25 @@
 
 namespace ecs {
 
-ActionInitialization::ActionInitialization(const G4String& outputFile,
+ActionInitialization::ActionInitialization(G4String const& outputFile,
 		Detector& aDetector) :
 		G4VUserActionInitialization(), fOutputFileSpec(outputFile), fDetector(
 				aDetector) {
 }
 
 void ActionInitialization::BuildForMaster() const {
+
 	SetUserAction(new RunAction(fOutputFileSpec));
+
 }
 
 void ActionInitialization::Build() const {
+
 	auto const runAction = new RunAction(fOutputFileSpec);
 	SetUserAction(runAction);
 	SetUserAction(new PrimaryGeneratorAction);
 	SetUserAction(new SteppingAction(fDetector, *runAction));
+
 }
 
 }
